e55dac4f3ec9d9f0b7d7a14abafa888271dc6773
Shanu <mdshahbuddin9504@gmail.com>
2024-02-04 09:51:11 +0530

    All about Server, file handling and also Understand Url

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -1,15 +1,27 @@
 // Use for Create a server
 const http = require("http");
-const PORT = 8800
+
+// Create a Port Variable
+const PORT = 8800;
 
 // use for file handling
 const fs = require('fs');
 
+// Use for Create a URL and it store in a url name variable.
+const url = require("url");
+
 const useServer = http.createServer((request, response) => {
+
+    // Basically it doesn't generate favicon.ico log file
+    if(request.url === '/favicon.ico') return response.end();
     
-    const log = `${Date.now()} : ${request.url} : New Request Recieved.\n`
+    const log = `${Date.now()} : ${request.url} : New Request Recieved.\n`;
+
+    const myUrl = url.parse(request.url, true);
+    console.log(myUrl)
+
     fs.appendFile('log.txt', log, (error, data) => {
-        switch (request.url) {
+        switch (myUrl.pathname) {
             case '/':
                 response.end("Hello Sabar!!");
                 break;
@@ -21,7 +33,15 @@ const useServer = http.createServer((request, response) => {
                 break;
             case '/bacha':
                 response.end("I Miss You Bacha <3");
-                break;        
+                break;    
+            case '/breakup':
+                const reason = myUrl.query.breakupreason
+                response.end(`Hey : ${reason} - but why Bacha ?`);
+                break;
+            case '/user':
+                const userInfo = myUrl.query.username
+                response.end(`Hello ${userInfo}`);
+                break;
             default:
                 response.end("404 not Found.")
         }
